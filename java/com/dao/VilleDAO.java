3f8153176d3dd60370f0dce899ec43431f3f2f73
killian Vallerie <killian.vallerie@reseau.eseo.fr>
2020-01-21 13:55:19 +0100

    adding code to work with city and not train

---
+++ java/com/dao/VilleDAO.java
@@ -0,0 +1,17 @@
+package com.dao;
+
+import java.util.ArrayList;
+
+import com.dto.Ville;
+
+public interface VilleDAO {
+	
+	public ArrayList<Ville> findVilles();
+
+    public ArrayList<Ville> findVilleCodePostal(String codePostal);
+
+	public void saveVille(Ville ville);
+	
+	public void updateVille(String codePostal, Ville ville);
+
+}
